e21fd8b1a614e14fab694fa094b22302319b2e02
Brenno Prettyman <brennoprettyman@gmail.com>
2023-02-18 13:36:45 -0300

    a

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Contagem_Regressiva;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine(" Preparar...");
+        Console.ReadKey();
+        Console.WriteLine("Apontar...");
+        Console.ReadKey();
+        Console.WriteLine("FOGO!!!");
+    }
+}
